0c21dc9c39baa8542d3f69ebe73ae59f22d7e3b7
zarakM <mughalzarak@gmail.com>
2020-01-26 20:39:25 -0800

    Redux implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+// import reducer from './reducers'
+// import middleware from './middleware'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './components/App'
+import { BrowserRouter } from 'react-router-dom';
 
+// const store = createStore(reducer, middleware);
+const store = {};
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);
